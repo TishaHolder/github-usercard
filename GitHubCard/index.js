079f02d3241e6b76fbf4542aabd80dafb2fea7c3
Tisha Holder <50963507+TishaHolder@users.noreply.github.com>
2019-08-08 02:55:48 -0400

    completed MVP

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,6 +3,24 @@
            https://api.github.com/users/<your name>
 */
 
+//select the cards container that will hold all the cards retrieved from github and returned by the cardCreator function
+let cardsContainer = document.querySelector(".cards");
+
+axios.get("https://api.github.com/users/TishaHolder")
+    .then( response => {      
+
+        // deal with the response data in here
+        console.log(response.data);  
+
+        const newGitHubRecord = cardCreator(response.data);
+        cardsContainer.appendChild(newGitHubRecord);         
+       
+    })
+    .catch( err => {
+        // deal with the error in here
+        console.log("Error:", err);
+    })
+
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
@@ -14,6 +32,8 @@
            create a new component and add it to the DOM as a child of .cards
 */
 
+
+
 /* Step 5: Now that you have your own card getting added to the DOM, either 
           follow this link in your browser https://api.github.com/users/<Your github name>/followers 
           , manually find some other users' github handles, or use the list found 
@@ -24,7 +44,22 @@
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = ["tetondan", "dustinmyers", "justsml", "luishrd", "bigknell"];
+
+followersArray.forEach ( follower => {
+  axios.get(`https://api.github.com/users/${follower}`)
+  .then( response => {
+
+    const newGitHubRecord = cardCreator(response.data);
+    cardsContainer.appendChild(newGitHubRecord);
+  })
+  .catch( err => {
+    // deal with the error in here
+    console.log("Error:", err);
+})
+
+});
+
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -45,6 +80,62 @@ const followersArray = [];
 </div>
 
 */
+
+function cardCreator(userDataObject) {
+
+  //define new elements
+  const divCard = document.createElement("div");
+  divCard.classList.add("card");
+  
+      const imageCard = document.createElement("img");
+      imageCard.classList.add("card");
+      imageCard.src = userDataObject.avatar_url;
+
+      const divCardInfo = document.createElement("div");
+      divCardInfo.classList.add("card-info");
+
+          const headerH3 = document.createElement("h3");
+          headerH3.classList.add("name");
+          headerH3.textContent = userDataObject.name;
+
+          const paragraphUserName = document.createElement("p");
+          paragraphUserName.classList.add("username");
+          paragraphUserName.textContent = userDataObject.login;
+
+          const paragraphLocation = document.createElement("p");
+          paragraphLocation.textContent = userDataObject.location;
+          
+          const paragraphProfile = document.createElement("p");
+          paragraphProfile.textContent = "Profile: ";
+              const linkProfile = document.createElement("a");
+              linkProfile.setAttribute("href", userDataObject.html_url);
+              linkProfile.textContent = userDataObject.html_url;
+
+          const paragraphFollowers = document.createElement("p");
+          paragraphFollowers.textContent = userDataObject.followers;
+
+          const paragraphFollowing = document.createElement("p");
+          paragraphFollowing.textContent = userDataObject.following;
+
+          const paragraphBio = document.createElement("p");
+          paragraphBio.textContent = userDataObject.bio;  
+          
+  //add elements to parent containers
+  divCard.appendChild(imageCard);
+  divCard.appendChild(divCardInfo);
+  
+      divCardInfo.appendChild(headerH3);
+      divCardInfo.appendChild(paragraphUserName);
+      divCardInfo.appendChild(paragraphLocation);
+      divCardInfo.appendChild(paragraphProfile);
+          paragraphProfile.appendChild(linkProfile);
+      divCardInfo.appendChild(paragraphFollowers);
+      divCardInfo.appendChild(paragraphFollowing);
+      divCardInfo.appendChild(paragraphBio);
+
+      return divCard;    
+    
+}//end cardCreator
 
 /* List of LS Instructors Github username's: 
   tetondan
